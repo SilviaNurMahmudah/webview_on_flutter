35aae6aaf4bef54f38bbf672ea9b17620f2d2774
SilviaNurMahmudah <silvia.mahmudah@gmail.com>
2022-09-18 15:22:00 +0700

    Memproses peristiwa pemuatan halaman

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ SILVIA NUR MAHMUDAH
 
 ### Menambahkan widget WebView ke Aplikasi Flutter
 ![Screenshot webview_on_flutter](images/01.png)
+
+### Memproses peristiwa pemuatan halaman
+![Screenshot webview_on_flutter](images/02.png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'src/web_view_stack.dart';
 
 void main() {
   runApp(
@@ -17,24 +17,14 @@ class WebViewApp extends StatefulWidget {
   State<WebViewApp> createState() => _WebViewAppState();
 }
 
-class _WebViewAppState extends State<WebViewApp> {
-  @override
-  void initState() {
-    if (Platform.isAndroid) {
-      WebView.platform = SurfaceAndroidWebView();
-    }
-    super.initState();
-  }
-  
+class _WebViewAppState extends State<WebViewApp> {  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter WebView'),
       ),
-      body: const WebView(
-        initialUrl: 'https://flutter.dev',
-      ),
+      body: const WebViewStack(),
     );
   }
 }
